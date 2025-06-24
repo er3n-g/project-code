46b8b556319ddb80b88e34193111298cb7ac643f
er3n-g <supercatsgaming0@gmail.com>
2025-06-25 01:25:37 +0300

    Update Arduino.c

--- Arduino.c
+++ Arduino.c
@@ -1,30 +1,28 @@
-#include <RTClib.h>
 #include <SoftwareSerial.h>
-#include <Wire.h>
- 
 
+SoftwareSerial ABC(11, 12); // RX = 11, TX = 12
 
-RTC_DS3231 rtc;
-int lastExecutedDay = -1;
+#define ENA 10
+#define inPin1 8
+#define inPin2 9
+#define inPin3 6
+#define inPin4 7
+#define ENB 5
+#define MoistPin A4
 
-SoftwareSerial ABC(9, 10); // RX = 9, TX = 10
-
-int inPin1 = 5;
-int inPin2 = 6;
-char plant[20];
+char incoming[30];
+char selectedPlant[30] = "";  // Store selected plant globally
+int PumpHaveActed = 0;
+int DaysPassed = 0;
 
 void setup() {
-  // -- clock -- //
-    rtc.begin();
-    if (rtc.lostPower()) {
-      rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Set RTC to compile time
-  }
-  // ----------- //
-  pinMode(13,OUTPUT);
-  Serial.begin(115200);      
+  pinMode(13, OUTPUT);
+  pinMode(MoistPin, INPUT);
+  Serial.begin(115200);
   ABC.begin(9600);
+  pinMode(ENA, OUTPUT);
   pinMode(inPin1, OUTPUT);
-  pinMode(inPin2, OUTPUT); 
+  pinMode(inPin2, OUTPUT);
   digitalWrite(inPin2, LOW);
   Serial.println("Arduino Ready");
 }
@@ -36,55 +34,105 @@ void trimNewline(char* str) {
   }
 }
 
-
-  // -- pump-- //
-void ActPump(int mlPerDay) {
-  int WaterTime = (mlPerDay * 16);
+void ActWaterPump(int WmlPerDay) {
+  int WwaterTime = WmlPerDay * 16;
+  analogWrite(ENA, 50);
   digitalWrite(inPin1, HIGH);
-  delay(WaterTime * 600); 
+  delay(WwaterTime * 600);
   digitalWrite(inPin1, LOW);
 }
-  //-----------//
+
+void ActFertPump(int FmlPerDay) {
+  int FwaterTime = FmlPerDay * 16;
+  analogWrite(ENB, 50);
+  digitalWrite(inPin3, HIGH);
+  delay(FwaterTime * 600);
+  digitalWrite(inPin3, LOW);
+}
+
 
 void loop() {
-  // -- clock -- //
-    DateTime now = rtc.now();
-
-    if (now.hour() == 12 && lastExecutedDay != now.day()) {
-      Plant(); // Your 24-hour task
-      lastExecutedDay = now.day();
-  }
-    delay(60000);
-  //-------------//
-
-}
-  void Plant(){
-  // --recive plant from ESP--//
   if (ABC.available()) {
-    byte len = ABC.readBytesUntil('\n', plant, sizeof(plant) - 1);
-    plant[len] = '\0'; 
-    trimNewline(plant);
+    byte len = ABC.readBytesUntil('\n', incoming, sizeof(incoming) - 1);
+    incoming[len] = '\0';
+    trimNewline(incoming);
 
     Serial.print("Received: ");
-    Serial.println(plant);
-  }
-    if (strcmp(plant,"Mint") == 0){
-      ActPump(300);
-      
+    Serial.println(incoming);
+
+    if (strncmp(incoming, "TIME:", 5) == 0) {
+      char* timeStr = incoming + 5;
+      Serial.print("Parsed time: ");
+      Serial.println(timeStr);
+
+      int hour = atoi(strtok(timeStr, ":"));
+      int minute = atoi(strtok(NULL, ":"));
+      int SoilMoist = analogRead(MoistPin);
+
+      /////////// -- activate fertelizer pumpy every week -- ////////
+        if (DaysPassed >= 7){
+          if (strcmp(selectedPlant, "Mint") == 0) {
+            ActFertPump(1);
+            DaysPassed = 0;
+          }
+          else if (strcmp(selectedPlant, "Rosemary") == 0){
+            ActFertPump(2);
+            DaysPassed = 0;
+          }
+          else if (strcmp(selectedPlant, "Lettuce") == 0) {
+            ActFertPump(3);
+            DaysPassed = 0;
+          }
+          else if (strcmp(selectedPlant, "Cherry Tomatoes") == 0) {
+            ActFertPump(3);
+            DaysPassed = 0;
+          }
+          else if (strcmp(selectedPlant, "Petunias") == 0) {
+            ActFertPump(5);
+            DaysPassed = 0;
+          }
+        }
+      /////////////////////////////////////////////////////////////
+
+        
+      ////////// -- activate water pump every day -- ///////
+     // if (SoilMoist < 500) {
+        if (hour == 22 && minute == 30 && PumpHaveActed != 1) {
+          if (strcmp(selectedPlant, "Mint") == 0) {
+            ActWaterPump(1);
+            PumpHaveActed = 1;
+            DaysPassed = DaysPassed +1;
+          } else if (strcmp(selectedPlant, "Rosemary") == 0) {
+            ActWaterPump(2);
+            PumpHaveActed = 1;
+            DaysPassed = DaysPassed +1;
+          } else if (strcmp(selectedPlant, "Lettuce") == 0) {
+            ActWaterPump(3);
+            PumpHaveActed = 1;
+            DaysPassed = DaysPassed +1;
+          } else if (strcmp(selectedPlant, "Cherry Tomatoes") == 0) {
+            ActWaterPump(4);
+            PumpHaveActed = 1;
+            DaysPassed = DaysPassed +1;
+          } else if (strcmp(selectedPlant, "Petunias") == 0) {
+            ActWaterPump(5);
+            PumpHaveActed = 1;
+            DaysPassed = DaysPassed +1;
+          } 
+        }
+        if (hour == 22 && minute == 31){
+          PumpHaveActed = 0;
+        }
+      //}
+      //////////////////////////////////////////////////////////
+    } else {
+      // Store selected plant
+      strncpy(selectedPlant, incoming, sizeof(selectedPlant) - 1);
+      selectedPlant[sizeof(selectedPlant) - 1] = '\0';
+      Serial.print("Selected Plant Set To: ");
+      Serial.println(selectedPlant);
     }
-    if (strcmp(plant,"Rosemary") == 0){
-      ActPump(180);
-    }
-    if (strcmp(plant,"Lettuce") == 0){
-      ActPump(20);
-    }
-    if (strcmp(plant,"Cherry Tomatoes") == 0){
-      ActPump(5);
-    }
-    if (strcmp(plant,"Petunias") == 0){
-      ActPump(80);
-    }
-    ///////////////////////////
-    
   }
 
+  delay(100);
+}
